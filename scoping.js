6bca5cfede120f207a0b018ec06e37261addc40a
pranavsharma55 <pranav.sharma55@gmail.com>
2016-12-30 12:37:35 +0530

    example about scoping

---
+++ scoping.js
@@ -0,0 +1,24 @@
+var a=7;
+console.log(a)
+
+function add()
+
+{
+
+var a;
+var b;
+
+
+var a=3;
+var b=6;
+console.log(a);
+console.log(b);
+
+}
+
+var c=8;
+var d=9;
+
+add();
+console.log(c)
+console.log(d)
